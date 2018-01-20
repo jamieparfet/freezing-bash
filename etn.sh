c286e290f993506d38af5b54f52c382585d2846a
jamieparfet <9058983+jamieparfet@users.noreply.github.com>
2018-01-19 22:15:34 -0500

    update with threads

--- etn.sh
+++ etn.sh
@@ -5,7 +5,7 @@ usage () {
     echo -e "Example: ./etn.sh -n google2 -p minekitten"
     echo -e "Options:"
     echo -e "  -n    Name of the server (to be used in tmux window)"
-    echo -e "  -p    Name of the pool (minekitten or easyhash)"
+    echo -e "  -t    Number of threads"
     echo
 }
 
@@ -13,23 +13,19 @@ usage () {
 while getopts ":n:p:" option; do
     case "${option}" in
         n) name=${OPTARG};;
-		p) pool=${OPTARG};;
+	t) threads=${OPTARG};;
         *) usage; exit;;
     esac
 done
 shift "$((OPTIND-1))"
 
 # Check if arguments are empty
-if [ -z "$name" ] || [ -z "$pool" ]; then
+if [ -z "$name" ] || [ -z "$threads" ]; then
     # Print help menu if they are empty
     usage
 else
 
-	if [[ "$pool" = "minekitten" ]]; then
-		stratum="etnpool.minekitten.io:3333"
-	elif [[ "$pool" = "easyhash" ]]; then
-		stratum="etn.easyhash.io:3630"
-	fi
+	stratum="etn.easyhash.io:3630"
 
 	# apt update & apt upgrade
 	sudo apt update
@@ -57,9 +53,9 @@ else
 	# Create tmux session with server name
 	tmux new -d -s "$name"
 	# Create window in new session with pool name
-	tmux neww -d -n cpu-"$pool" -t "$name"
+	tmux neww -d -n cpu-"$threads" -t "$name"
 	# Send miner command to window using stratum variable to specify pool
-	tmux send -t "$name":cpu-"$pool" "sudo minerd -a cryptonight -o stratum+tcp://$stratum -u etnkNshfGNVTZbF2919Gv84yXs14Bn1Ys41VMJ73izbajQ6fkJAFrzQSLeX5wmWtDMZ43P4BAPGVZZnuTkFcqHm74oQELZdcSK -p x" ENTER
+	tmux send -t "$name":cpu-"$threads" "sudo minerd -a cryptonight -o stratum+tcp://$stratum -u etnkNshfGNVTZbF2919Gv84yXs14Bn1Ys41VMJ73izbajQ6fkJAFrzQSLeX5wmWtDMZ43P4BAPGVZZnuTkFcqHm74oQELZdcSK -p x -t $threads" ENTER
 
 	# Clean up tmux, kill empty bash window
 	tmux killw -t "$name":1
